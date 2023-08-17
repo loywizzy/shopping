6cd7163a2dbf91caff1eabaae5927c5e124a3085
loywizzy <kdftvguitarandarmy@gmail.com>
2023-08-17 15:30:03 +0700

    v3

--- js/shop.js
+++ js/shop.js
@@ -78,8 +78,15 @@ function updateTotalPrice() {
     var totalPrice4 = counter4 * pricePerUnit4;
     var unitzz = counter1 + counter2 +counter3 +counter4;
     var total = totalPrice1 + totalPrice2 + totalPrice3 + totalPrice4;
+    var discount = 0;
+
+    if (total >= 1000) {
+        discount = total * 0.1
+        total *= 0.9;
+    }
     document.getElementById("totalPrice").innerText = total;
     document.getElementById("unitzz").innerText = unitzz;
+    document.getElementById("discount").innerText = discount.toFixed(2);
 }
 
 function finalizeOrder() {
@@ -89,4 +96,4 @@ function finalizeOrder() {
 
     document.getElementById("unitzz").textContent = "0";
     document.getElementById("totalPrice").textContent = "0";
-    }ห
+    }
